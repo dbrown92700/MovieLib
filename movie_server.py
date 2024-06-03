e07fb253cf823824168db66d1ffe166a35fd97c1
David Brown <dbrown92700@gmail.com>
2024-06-02 17:50:18 -0700

    sql updates

--- movie_server.py
+++ movie_server.py
@@ -113,7 +113,7 @@ def list_movies():
         available_radio += f'<td>{choice.title()} <input type="radio" name="available" ' \
                            f'value="{choice}" {selected}></td>'
 
-    top250_radio = f'<td><input type="radio" name="top250" value="True" {"checked" if top250=="True" else ""}></td>'
+    top250_radio = f'<input type="radio" name="top250" value="True" {"checked" if top250=="True" else ""}>'
 
     url = (f'{app_url}/?name={"+".join(name)}&genre={genre}&watched={watched}&available={available}&'
            f'rating={rating}&top250={"True" if top250=="True" else ""}&sort={sort}&direction={direction}')
