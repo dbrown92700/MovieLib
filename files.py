62ebce2b54e7b939209166793c8ca41de7013f0a
David Brown <dbrown92700@gmail.com>
2024-06-06 19:43:56 -0700

    sql updates

--- files.py
+++ files.py
@@ -17,25 +17,63 @@ class Files:
 
     def delta(self, comp_dirs_list: dict):
 
-        files_delta = {
-            'added': {},
-            'removed': {}
-        }
+        files_delta = {}
         for directory in self.dirs:
             file_list = self.dirs[directory]
             try:
                 comp_files = comp_dirs_list[directory]
             except KeyError:
-                files_delta['added'][directory] = file_list
-                continue
-            added = [x for x in comp_files if x not in file_list]
-            if added:
-                files_delta['removed'][directory] = added
-            removed = [x for x in file_list if x not in comp_files]
-            if removed:
-                files_delta['added'][directory] = removed
+                comp_files = []
+            removed = [x for x in comp_files if x not in file_list]
+            for f in removed:
+                if f in files_delta.keys():
+                    if files_delta[f]['action'] == 'removed':
+                        continue
+                    else:
+                        files_delta[f]['action'] = 'moved'
+                        files_delta[f]['dir'] = directory
+            added = [x for x in file_list if x not in comp_files]
+            for f in added:
+                if f in files_delta.keys():
+                    if files_delta[f]['action'] == 'added':
+                        continue
+                    else:
+                        files_delta[f]['action'] = 'moved'
+                        files_delta[f]['dir'] = directory
         for directory in comp_dirs_list:
             if directory not in self.dirs.keys():
-                files_delta['removed'][directory] = comp_dirs_list[directory]
+                for f in comp_dirs_list[directory]:
+                    if f in files_delta.keys():
+                        if files_delta[f]['action'] == 'removed':
+                            continue
+                        else:
+                            files_delta[f]['action'] = 'moved'
+                            files_delta[f]['dir'] = directory
 
         return files_delta
+
+    # def delta(self, comp_dirs_list: dict):
+    #
+    #     files_delta = {
+    #         'added': {},
+    #         'removed': {},
+    #         'moved': {}
+    #     }
+    #     for directory in self.dirs:
+    #         file_list = self.dirs[directory]
+    #         try:
+    #             comp_files = comp_dirs_list[directory]
+    #         except KeyError:
+    #             files_delta['added'][directory] = file_list
+    #             continue
+    #         removed = [x for x in comp_files if x not in file_list]
+    #         if removed:
+    #             files_delta['removed'][directory] = removed
+    #         added = [x for x in file_list if x not in comp_files]
+    #         if added:
+    #             files_delta['added'][directory] = added
+    #     for directory in comp_dirs_list:
+    #         if directory not in self.dirs.keys():
+    #             files_delta['removed'][directory] = comp_dirs_list[directory]
+    #
+    #     return files_delta
